6b4ddb01de6257c00ecaefb99784d2e0312de575
jack <64680540+jacks429@users.noreply.github.com>
2025-05-21 12:34:18 +0000

    feat: properly implmenetd deployrs block

--- src/blockTypes/deployrsConfigurations.nix
+++ src/blockTypes/deployrsConfigurations.nix
@@ -9,6 +9,39 @@
     type = "deployrsConfiguration";
     transform = import ../transformers/deployrsConfigurations.nix;
     output = "deploy.nodes";
+
+    # Optional: Add actions for testing/deployment
+    actions = {
+      currentSystem,
+      fragment,
+      fragmentRelPath,
+      target,
+      inputs,
+    }: let
+      inherit (root) mkCommand;
+    in [
+      (mkCommand currentSystem {
+        name = "check";
+        description = "Check deploy-rs configuration";
+        command = ''
+          ${inputs.deploy-rs.packages.${currentSystem}.deploy-rs}/bin/deploy check
+        '';
+      })
+      (mkCommand currentSystem {
+        name = "dry-run";
+        description = "Perform a dry-run deployment";
+        command = ''
+          ${inputs.deploy-rs.packages.${currentSystem}.deploy-rs}/bin/deploy .#${target} --dry-run
+        '';
+      })
+      (mkCommand currentSystem {
+        name = "deploy";
+        description = "Deploy the configuration";
+        command = ''
+          ${inputs.deploy-rs.packages.${currentSystem}.deploy-rs}/bin/deploy .#${target}
+        '';
+      })
+    ];
   };
 in
   deployrsConfigurations
