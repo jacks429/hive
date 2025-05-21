aa113eaa66c925aa29951306ba6fe40a355fd3ee
jack <64680540+jacks429@users.noreply.github.com>
2025-05-21 12:19:27 +0000

    feat: removed collector from deployrs

--- src/blockTypes/deployrsConfigurations.nix
+++ src/blockTypes/deployrsConfigurations.nix
@@ -7,7 +7,6 @@
   deployrsConfigurations = {
     name = "deployrsConfigurations";
     type = "deployrsConfiguration";
-    collect = import ../collectors/default.nix;
     transform = import ../transformers/deploy-rs-single.nix;
     output = "deploy.nodes";
   };
